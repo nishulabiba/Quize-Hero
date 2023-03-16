ccb4223c0e1acc5977b53bb23f28ab8b31231654
nishulabiba <nishlabiba2gmail.com>
2023-03-16 08:35:06 +0600

    fixed countdown

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ startQuiz.addEventListener("click", () => {
 
   let x = setInterval(() => {
     if (counterNum < 0) {
-      coutDown.classList.remove("flex");
-      coutDown.classList.add("hidden");
+      countDown.classList.remove("flex");   //fixed
+      countDown.classList.add("hidden");
       counterNum = 3;
       count = 0;
       timer = null;
@@ -47,8 +47,8 @@ startQuiz.addEventListener("click", () => {
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
   const data = await res.json;
-  quizData = data;
-  displayQuiz(data);
+  // quizData = data;
+  displayQuiz(data);  //fixed
 };
 
 // Displaying quiz on quiz page
@@ -58,7 +58,7 @@ const displayQuiz = (data) => {
     return;
   }
 
-  data.forEach((quiz, i) => {
+  data.forEach((quiz, i) => {  // fixed
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
@@ -74,7 +74,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
